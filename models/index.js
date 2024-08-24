4d9109e4db2b56ebc458182a6e6fb6a1948f708d
cj4871 <146256795+cj4871@users.noreply.github.com>
2024-08-24 01:03:49 -0500

    added category models

--- models/index.js
+++ models/index.js
@@ -3,15 +3,26 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { model } = require('../config/connection');
 
 // Products belongsTo Category
-
+Product.belongsToMany(Category,{
+  foreignKey: 'model_id'
+})
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey: 'product_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag,{
+  foreignKey:''
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product,{
+  through:{
+    model: ProductTag
+  }
+})
 module.exports = {
   Product,
   Category,
